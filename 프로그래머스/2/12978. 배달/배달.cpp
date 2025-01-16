956a73a77f264142582cdb34e26370252086681a
KwakSeungHyub <rhkrtmdguq1@naver.com>
2025-01-16 19:58:33 +0900

    [level 2] Title: 배달, Time: 0.37 ms, Memory: 4.46 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,48 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-using namespace std;
-
-// 간선 정보
-struct Edge {
-    int from, to, weight;
-};
-
-int solution(int N, vector<vector<int>> road, int K) {
-    vector<Edge> edges;
-    vector<int> dist(N + 1, INT_MAX);
-
-    // 그래프 정보 초기화 (무방향 그래프로 양측 노드에 삽입)
-    for (vector<int> r : road) {
-        edges.push_back({ r[0], r[1], r[2] });
-        edges.push_back({ r[1], r[0], r[2] });
-    }
-
-    // 시작 노드의 거리는 0
-    dist[1] = 0; 
-
-    // 벨만-포드 알고리즘
-    // 모든 노드에 대한 가장 짧은 경로는 노드의 수보다 하나 작은 간선의 수(N-1)로 이루어지기에 N-1번 반복
-    for (int i = 0; i < N - 1; ++i)
-        for (Edge edge : edges) 
-        {
-            int from = edge.from;
-            int to = edge.to;
-            int weight = edge.weight;
-    
-            // 아직 다른 간선에 의해 비용이 계산되지 않은경우에는 무시
-            if(dist[from] == INT_MAX)
-                continue;
-            
-            // 현재 위치의 비용(dist[from]) + 다음 위치까지의 비용(weight)이 저장된 비용(dist[to])보다 작으면 갱신
-            dist[to] = min(dist[to], dist[from] + weight);
-        }
-    
-    // 정답 구하기
-    int answer = 0;
-    for(int i = 1; i <= N; ++i)
-        if(dist[i] <= K)
-            ++answer;
-
-    return answer;
+#include <vector>
+#include <queue>
+using namespace std;
+ 
+vector<pair<int,int>> V[55];
+vector<int> Dist;
+ 
+int Min(int A, int B) { if (A < B) return A; return B; }
+ 
+void Dijkstra(int N)
+{
+    priority_queue<pair<int, int>> PQ;
+    PQ.push(make_pair(0, 1));
+    Dist[1] = 0;
+ 
+    while (PQ.empty() == 0)
+    {
+        int Cost = -PQ.top().first;
+        int Cur = PQ.top().second;
+        PQ.pop();
+ 
+        for (int i = 0; i < V[Cur].size(); i++)
+        {
+            int Next = V[Cur][i].first;
+            int nCost = V[Cur][i].second;
+ 
+            if (Dist[Next] > Cost + nCost)
+            {
+                Dist[Next] = Cost + nCost;
+                PQ.push(make_pair(-Dist[Next], Next));
+            }
+        }
+    }
 }
+ 
+int solution(int N, vector<vector<int> > road, int K)
+{
+    Dist.resize(N + 1, 2e9);
+    for (int i = 0; i < road.size(); i++)
+    {
+        int N1 = road[i][0];
+        int N2 = road[i][1];
+        int Dist = road[i][2];
+ 
+        V[N1].push_back(make_pair(N2, Dist));
+        V[N2].push_back(make_pair(N1, Dist));
+    }
+ 
+    Dijkstra(N);
+    int answer = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (Dist[i] <= K) answer++;
+    }
+ 
+    return answer;
+}
+
+
