16e83fefc030eb52890f7d8d8ad78ffe2c24124a
KwakSeungHyub <rhkrtmdguq1@naver.com>
2025-01-16 19:23:49 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 13.22 ms, Memory: 7.18 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
@@ -1,44 +1,61 @@
 #include <string>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+int board[104][104];
+vector<int> solution(int r, int c, vector<vector<int>> q) {
+    
     vector<int> answer;
-
-    int arr[101][101];
-    int side[4][2] = { {1,0},{0,1},{-1,0},{0,-1} };
-
-    for (int i = 1; i <= rows; i++)
-        for (int j = 1; j <= columns; j++)
-            arr[i][j] = ((i - 1) * columns) + j;
-
-    for (int i = 0; i < queries.size(); i++)
-    {
-        int dir = 0;
-        int minNum = 10010;
-        int y = queries[i][0];
-        int x = queries[i][1];
-        int firstNum = arr[y][x];
-
-        while (y != queries[i][0] || x != queries[i][1] + 1)
-        {
-            if(y==queries[i][2]&&x==queries[i][1])
-                dir++;
-            else if(y==queries[i][2]&&x==queries[i][3])
-                dir++;
-            else if(y==queries[i][0]&&x==queries[i][3])
-                dir++;
-            
-            arr[y][x] = arr[y + side[dir][0]][x + side[dir][1]];
-            y += side[dir][0];
-            x += side[dir][1];
-            minNum = min(minNum, arr[y][x]);
+    int cnt = 1;
+    for(int i=1; i<= r; i++){
+        for(int j=1; j<=c; j++){
+            board[i][j] = cnt;
+            cnt++;
         }
-        arr[y][x] = firstNum;
-        minNum = min(minNum, arr[y][x]);
-        answer.push_back(minNum);
     }
-
+    for(int i=0; i<q.size(); i++){
+        vector<int> tmp;
+        vector<pair<int,int>> xy;
+        int mn = 1e9;
+        int sty = q[i][0];
+        int stx = q[i][1];
+        int eny = q[i][2];
+        int enx = q[i][3];
+        // 오른쪽
+        for(int i=stx; i<=enx; i++){
+            tmp.push_back(board[sty][i]);
+            mn = min(mn, board[sty][i]);
+            xy.push_back({sty,i});
+        }
+        // 아래
+        for(int i=sty+1; i<=eny; i++){
+            tmp.push_back(board[i][enx]);
+            mn = min(mn, board[i][enx]);
+            xy.push_back({i, enx});
+        }
+        
+        // 왼쪽
+        for(int i=enx-1; i>= stx; i--){
+            tmp.push_back(board[eny][i]);
+            mn = min(mn, board[eny][i]);
+            xy.push_back({eny, i});
+        }
+        
+        // 위
+        for(int i=eny-1; i>= sty+1; i--){
+            tmp.push_back(board[i][stx]);
+            mn = min(mn, board[i][stx]);
+            xy.push_back({i, stx});
+        }
+        
+        rotate(tmp.rbegin(), tmp.rbegin()+1, tmp.rend());
+        
+        for(int i=0; i<xy.size(); i++){
+            board[xy[i].first][xy[i].second] = tmp[i];
+        }
+        
+        answer.push_back(mn);
+    }
     return answer;
 }
